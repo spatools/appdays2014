2188a8022479b2636ac2c600b2a8146d488347a0
Maxime LUCE <maxime@touchify.co>
2014-11-06 21:20:27 +0100

    Append an initializer module to manage app startup

---
+++ src/app/initializer.js
@@ -0,0 +1,18 @@
+﻿define(['promise'], function (Promise) {
+    function base() {
+        return new Promise(function (resolve) {
+            require(
+                ['ui/ui'],
+                function () { resolve(); }
+            );
+        });
+    }
+
+    function initialize() {
+        return base();
+    }
+
+    return {
+        initialize: initialize
+    };
+});
